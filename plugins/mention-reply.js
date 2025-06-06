5b7b0914018d396e6709319b1c41cde877ac561d
TINTUMOWN <tintumown7@gmail.com>
2025-06-06 20:10:12 +0530

    Update mention-reply.js

--- plugins/mention-reply.js
+++ plugins/mention-reply.js
@@ -10,7 +10,7 @@ malvin({
     if (!m.mentionedJid || m.mentionedJid.length === 0) return;
 
     const voiceClips = [
-      "https://cdn.ironman.my.id/i/7p5plg.mp4",
+      "https://files.catbox.moe/2qzq0k.mp4",
       "https://cdn.ironman.my.id/i/l4dyvg.mp4",
       "https://cdn.ironman.my.id/i/4z93dg.mp4",
       "https://cdn.ironman.my.id/i/m9gwk0.mp4",
